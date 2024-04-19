6924201e73740c0bbdfdfe92afe0e41a6f3911b2
FarihaTanha <121506178+FarihaTanha@users.noreply.github.com>
2024-04-19 14:24:04 +0600

    Update login.php

--- login.php
+++ login.php
@@ -16,37 +16,60 @@ if (isset($_SESSION["user"])) {
 </head>
 <body>
     <div class="container">
+        <!-- // logo section -->
+        <div class="d-flex justify-content-center justify-items-center w-50 m-auto">
+            <a href="index.php">
+            <img src="assets/images/logo.png" alt="logo" class="img-fluid">
+            </a>
+        </div>
         <?php
         if (isset($_POST["login"])) {
            $email = $_POST["email"];
            $password = $_POST["password"];
+           $nid = $_POST["nid_card"];
             require_once "database.php";
-            $sql = "SELECT * FROM users WHERE email = '$email'";
-            $result = mysqli_query($conn, $sql);
-            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            if ($user) {
-                if (password_verify($password, $user["password"])) {
-                    session_start();
-                    $_SESSION["user"] = "yes";
-                    header("Location: index.php");
-                    die();
+            
+            // Check if NID exists in the database
+            $sql_nid_check = "SELECT * FROM users WHERE nid_card = '$nid'";
+            $result_nid_check = mysqli_query($conn, $sql_nid_check);
+            $nid_exists = mysqli_num_rows($result_nid_check) > 0;
+            
+            if ($nid_exists) { // NID exists in the database
+                $sql = "SELECT * FROM users WHERE email = '$email'";
+                $result = mysqli_query($conn, $sql);
+                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                if ($user) {
+                    if (password_verify($password, $user["password"])) {
+                        session_start();
+                        $_SESSION["user"] = "yes";
+                        header("Location: index.php");
+                        die();
+                    }else{
+                        echo "<div class='alert alert-danger'>Password does not match</div>";
+                    }
                 }else{
-                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                    echo "<div class='alert alert-danger'>Email does not match</div>";
                 }
-            }else{
-                echo "<div class='alert alert-danger'>Email does not match</div>";
+            } else { // NID does not exist in the database
+                echo "<div class='alert alert-danger'>Invalid NID number</div>";
             }
         }
         ?>
       <form action="login.php" method="post">
         <div class="form-group">
-            <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" name="email" placeholder="johndoe@example.com" required>
         </div>
         <div class="form-group">
-            <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+            <label for="email">NID Card Number:</label>
+            <input type="text" class="form-control" name="nid_card" placeholder="1971XXXXXXXXXXXXX" required>
+        </div>        
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" name="password" placeholder="***********" required>
         </div>
         <div class="form-btn">
-            <input type="submit" value="Login" name="login" class="btn btn-primary">
+            <input type="submit" class="btn" style="background-color:#9d0858;color:#ffffff" value="Register" name="submit">
         </div>
       </form>
      <div><p>Not registered yet <a href="registration.php">Register Here</a></p></div>
